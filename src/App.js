259ade43b7426eeb58f6fea2597d897deadd6850
github username <jeremyagottfried@gmail.com>
2018-05-24 17:51:49 -0400

    added login form

--- src/App.js
+++ src/App.js
@@ -4,21 +4,45 @@ import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-    fetch('http://localhost:4000/login', {
-      method: 'POST',
-      body:
-        JSON.stringify({
-          username: 'jeremy',
-          password: 'password'
-        })
-      ,
-      headers: {
-        'Content-Type': "application/json",
-        'accepts': 'application/json',
-      }
-    }).then(res => res.json()).then(console.log)
+  state = {
+    username: '',
+    password: ''
   }
+
+  componentDidMount() {
+
+  }
+
+doFetch = ({username, password}) => {
+  fetch('http://localhost:4000/login', {
+    method: 'POST',
+    body:
+      JSON.stringify({
+        username: username,
+        password: password
+      })
+    ,
+    headers: {
+      'Content-Type': "application/json",
+      'accepts': 'application/json',
+    }
+  }).then(res => res.json()).then(console.log)
+}
+
+  handleSubmit = (event) => {
+  event.preventDefault();
+  const { username, password } = this.state;
+  const auth = { username, password };
+  this.doFetch(auth)
+}
+
+// insert fetch here
+
+handleChange = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
 
   render() {
     return (
@@ -30,6 +54,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+          <div>
+            <form onSubmit={ this.handleSubmit }>
+              <label htmlFor="username">Username</label>
+              <input type="text"
+                onChange={ this.handleChange }
+                value={ this.state.username }
+                name="username"
+                id="username" />
+              <label htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                onChange={ this.handleChange }
+                value={ this.state.password }
+                name="password"
+                id="password" />
+              <input type="submit" />
+            </form>
+          </div>
       </div>
     );
   }
