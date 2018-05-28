1c8cc3939fe54eb3aba5a4e579a3fdd539f25d60
github username <jeremyagottfried@gmail.com>
2018-05-28 17:23:06 -0400

    index.js actioncable handler

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+<ActionCableProvider url={API_WS_ROOT}>
+  <Router>
+  <App />
+  </Router>
+  </ActionCableProvider>, document.getElementById('root'));
 registerServiceWorker();
